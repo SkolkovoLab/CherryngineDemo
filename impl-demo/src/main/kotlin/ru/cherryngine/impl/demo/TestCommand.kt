c985daea07bb6fe7f6841355bf006c4e2b3c5ab2
MelonHell <MelonHell@ya.ru>
2026-04-13 02:01:11 +0300

    /phys cube фикс контекстов

--- impl-demo/src/main/kotlin/ru/cherryngine/impl/demo/TestCommand.kt
+++ impl-demo/src/main/kotlin/ru/cherryngine/impl/demo/TestCommand.kt
@@ -90,13 +90,17 @@ class TestCommand(
         ecsWorld.commandAction {
             val playerEntity = playerIndex.getOrThrow(sender.uuid)
             val spawnPosition = playerEntity[PositionComponent].position
-            val contexts = playerEntity[PlayerComponent].viewContextIDs
+
+            // куб видит то, что видит игрок
+            val viewContexts = playerEntity[PlayerComponent].viewContextIDs
+            // куб видят те, кто видит игрока
+            val viewableContexts = playerEntity[ViewableComponent].viewContextIDs
 
             entity {
-                it += PhysicsComponent(bodyInfo = PhysicsComponent.BodyInfo.Cube, physContextIDs = contexts)
+                it += PhysicsComponent(bodyInfo = PhysicsComponent.BodyInfo.Cube, physContextIDs = viewContexts)
                 it += PositionComponent(spawnPosition)
                 it += CubeModelComponent(material = Key.key("tnt"), transform = Transform())
-                it += ViewableComponent(contexts)
+                it += ViewableComponent(viewableContexts)
             }
         }
     }
